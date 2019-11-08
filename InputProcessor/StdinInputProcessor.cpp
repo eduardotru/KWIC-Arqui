8d983e548dbb18498c37b3f52de97ff07bbf2397
Diego Valadez <the-vala@users.noreply.github.com>
2019-11-07 20:58:49 -0600

    modified stdinputprocessot to count lines

--- InputProcessor/StdinInputProcessor.cpp
+++ InputProcessor/StdinInputProcessor.cpp
@@ -5,8 +5,15 @@
 
 AbstractWordsContainer* StdinInputProcessor::read(AbstractWordsContainer* words) {
     std::string sentence;
-    while(getline(std::cin, sentence)) {
+    int lines;
+
+    std::cout << "Cuantas lineas se van a ingresar?\n";
+    std::cin >> lines;
+
+    for (int i = 0; i < lines; i++) {
+        getline(std::cin, sentence);
         words->addSentence(sentence);
     }
+
     return words;
 }
