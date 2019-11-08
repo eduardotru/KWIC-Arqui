9522f1086601867f8edcc8266fa481cee002321d
Eduardo Trujillo <edytr1997@gmail.com>
2019-11-07 18:34:32 -0600

    Add BubbleSorter

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "InputProcessor/StdinInputProcessor.h"
 #include "WordProcessor/WordProcessor.h"
 #include "RotationProcessor/RotationProcessor.h"
-#include "Sorter/STLSorter.h"
+#include "Sorter/BubbleSorter.h"
 #include "OutputProcessor/StdoutOutputProcessor.h"
 #include "WordsContainer/VectorWordsContainer.h"
 
@@ -10,7 +10,7 @@ int main() {
     words = StdinInputProcessor().read(words);
     words = WordProcessor().processWords(words);
     words = RotationProcessor().rotate(words);
-    words = STLSorter().sort(words);
+    words = BubbleSorter().sort(words);
     words = StdoutOutputProcessor().write(words);
     delete words;
     return 0;
